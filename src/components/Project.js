960c13ce0f42bd388fdca382d7cf9f3a0d4e1fb1
shuib <shuaib_97@hotmail.co.uk>
2023-03-09 22:59:31 +0000

    adding images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,27 +1,34 @@
-export default function Project() {
+import React from "react";
+
+import Project1 from "../images/project-1.jpeg";
+import Project2 from "../images/project-2.jpeg";
+import Project3 from "../images/project-3.jpeg";
+
+
+export default function project() {
     return (<section className="sub-section-extra" id="projects">
     <h2>Projects</h2>
-    <div class="project-container">
+    <div className="project-container">
     <div id="project-card-1" className="project-card">
-        <img className="project-image" src="./images/project-1.jpeg" alt="Project One Image"/>
+        <img className="project-image" src={Project1} alt="Project One Image"/>
                 <h3>Project One</h3>
                 <p className="subtext">Here you can find my first project that was completed a few weeks after I started my bootcamp</p>
                 <hr/>
-                <p class="subtext"><a class="project-link" >View here</a></p>
+                <p className="subtext"><a className="project-link" >View here</a></p>
     </div>
-    <div id="project-card-2" class="project-card">
-        <img class="project-image" src="./images/project-2.jpeg" alt="Project Two Image"/>
+    <div id="project-card-2" className="project-card">
+        <img className="project-image" src={Project2} alt="Project Two Image"/>
                 <h3>Project Two</h3>
-                <p class="subtext">Here you can find my second project that was completed a few months after I started my bootcamp.</p>
+                <p className="subtext">Here you can find my second project that was completed a few months after I started my bootcamp.</p>
                 <hr/>
-                <p class="subtext"><a class="project-link">View here</a></p>
+                <p className="subtext"><a className="project-link">View here</a></p>
     </div>
-    <div id="project-card-3" class="project-card">
-        <img class="project-image" src="./images/project-3.jpeg" alt="Project Three Image"/>
+    <div id="project-card-3" className="project-card">
+        <img className="project-image" src={Project3} alt="Project Three Image"/>
                 <h3>Project Three</h3>
-                <p class="subtext">Here you can find my third project that was completed towards the end of my bootcamp.</p>
+                <p className="subtext">Here you can find my third project that was completed towards the end of my bootcamp.</p>
                 <hr/>
-                <p class="subtext"><a class="project-link">View here</a></p>
+                <p className="subtext"><a className="project-link">View here</a></p>
     </div>
     </div>
 </section>

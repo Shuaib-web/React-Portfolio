386ac142c95f97224ea9806c127c57923cae1dc3
shuib <shuaib_97@hotmail.co.uk>
2023-03-20 22:52:44 +0000

    revamp of entire portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,90 +1,52 @@
 import React from "react";
 
-import Project1 from "../images/Weather-Dashboard.png";
-import Project2 from "../images/Text-Editor.png";
-import Project3 from "../images/Note-Taker.png";
+import NoteTaker from "../images/Note-Taker.png";
+import TextEditor from "../images/Text-Editor.png";
 
+import "../project.css";
 
-export default function project() {
-return ( <div className="container">
-<div className="card">
-<img src={Project1} alt="Weather Dashboard"></img>
-<h2>
-  <strong>Weather Dashboard</strong>
-</h2>
-<div className="buttons">
-  <button className="code">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/Shuaib-web/Weather-app"
-    >
-      Code
-    </a>
-  </button>
-  <button className="site">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://shuaib-web.github.io/Weather-app/"
-    >
-      Site
-    </a>
-  </button>
-</div>
-</div>
-<div className="card">
-<img src={Project2} alt="Text Editor"></img>
-<h2>
-  <strong>Text Editor</strong>
-</h2>
-<div className="buttons">
-  <button className="code">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/Shuaib-web/Text-Editor"
-    >
-      Code
-    </a>
-  </button>
-  <button className="site">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://shuib-text-editor.herokuapp.com/"
-    >
-      Site
-    </a>
-  </button>
-</div>
-</div>
-<div className="card">
-<img src={Project3} alt="Note Taker"></img>
-<h2>
-  <strong>Note Taker</strong>
-</h2>
-<div className="buttons">
-  <button className="code">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/Shuaib-web/Note-Taker"
-    >
-      Code
-    </a>
-  </button>
-  <button className="site">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://shuib-note-taker.herokuapp.com/"
-    >
-      Site
-    </a>
-  </button>
-</div>
-</div>
-</div>
-    );
-}
+const Portfolio = () => {
+  return (
+    <div id="portfolio" data-aos="fade-in">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-11 offset-sm-1">
+            <div className="row">
+              <div className="col-sm-12">
+                <h2>Portfolio</h2>
+                <br />
+              </div>
+        
+              <div className="col-sm-4">
+                <div className="imgsec" data-aos="zoom-in">
+                  <a href="">
+                  <img src={NoteTaker} className="img-fluid" alt="Express.js: Note Taker" />
+                  <h6>Express.js: Note Taker</h6>
+                  </a>
+                </div>
+              </div>
+
+              <div className="col-sm-4">
+                <div className="imgsec" data-aos="zoom-in">
+                  <a href="https://ms-note-taker.herokuapp.com/">
+                  <img src={TextEditor} className="img-fluid" alt="Express.js: Note Taker" />
+                  <h6>Express.js: Text Editor</h6>
+                  </a>
+                </div>
+              </div>
+              
+              
+            
+            
+            
+              
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
